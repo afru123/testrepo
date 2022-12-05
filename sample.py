df80d27b5789f46b5fe44df4638b6b454d3e4c28
afru123 <afrasiabkhan432@yahoo.com>
2022-12-05 11:02:47 -0600

    The sample python program is added

---
+++ sample.py
@@ -0,0 +1,17 @@
+#python sample program for jenkins pipeline
+import os
+List1 = [8, 9, 4, 5, 7, 2]
+List1.reverse()
+print("The reverse lis is ", List1)
+
+List2 = [98, 76, 56, 34, 65, 33, 22, 99]
+List2.sort()
+print("The sorted list is ", List2)
+
+List3 = []
+List3 = List1.copy()
+print("List3 = ", List3)
+
+indexvalue = List2[2:6]
+print("The index values are ", indexvalue)
+
