84f20bb4cd128ecbb7f8d14981e5748dda404b78
unknown <afrasiabkhan432@yahoo.com>
2025-10-05 17:46:50 -0500

    we have added some new commands

--- nodeshealth.sh
+++ nodeshealth.sh
@@ -10,13 +10,23 @@
 ###################
 
 set -x # Debug mode
+set -e # exit the script when there is an error found.
+set -o pipefail 
 
+# the reason we used set -o pipefail is because the set -e is passed by the pipe '|' in line of code.
+# we can use the above three commands in online too i.e set -xeo but it is not recommended.
 df -h
 
-
 free -g
-
 
 nproc
 
 ps -ef | grep "amazon"   # grep command is used to filter specific word/words.
+
+# | sending the output of first command to the second one.
+# The only case | do not provide the output to the second comman is when the first command is "stdin". For example date | echo 'The time is', here the date command will not execute becuse it is standard input command
+
+ps -ef | grep "amazon" awk -F" " '{print $2}'   # this awk command is helping in pulling out the specific column, here we got info of 2nd column.
+
+
+
