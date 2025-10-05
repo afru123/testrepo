4813b1d77965498e9705cbe60fabc75a48e2a572
unknown <afrasiabkhan432@yahoo.com>
2025-10-05 14:00:06 -0500

    Nodeshealth script

---
+++ nodeshealth.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+###################
+# Author: Afrasiab Khan
+# Date : October 5, 2025
+#
+# This script outputs the nodes health
+#
+# Version: v1
+###################
+
+set -x # Debug mode
+
+df -h
+
+
+free -g
+
+
+nproc
+
+
